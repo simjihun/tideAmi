5924b2885a4cfd472036f387d3262d42c024b47c
simjihun <simering1989@gmail.com>
2023-07-24 16:44:24 +0900

    axios 설정

---
+++ front-end/src/axios/index.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:9000/';
+
+//axios.defaults.baseURL = '/api';
+// axios.defaults.headers.common.Accept = 'application/json';
+// axios.defaults.baseURL = 'http://localhost:9000/';
+// axios.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
+
+// Add a request interceptor
+axios.interceptors.request.use(
+    function (config) {
+        // Do something before request is sent
+        console.log('request interceptor!!!!');
+        return config;
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+    function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        console.log('response interceptor!!!!');
+        return response;
+    },
+    function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+    }
+);
+
+export default axios;
